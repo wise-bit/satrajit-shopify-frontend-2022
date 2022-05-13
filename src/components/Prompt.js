a10632d5d4e81ec14a9d808ccaaa87c33ff8ef31
wise-bit <satrajit2016@gmail.com>
2022-05-12 20:03:38 -0400

    added search button functionality

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,7 +1,15 @@
 import Box from '@mui/material/Box';
 import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import { useState } from 'react';
 
 export default function Prompt() {
+  const [input, setInput] = useState('');
+
+  const search = () => {
+    console.log(input);
+  };
+
   return (
     <>
       <div
@@ -13,8 +21,16 @@ export default function Prompt() {
         }}
       >
         <Box>
-          <h1>Fun with AI</h1>
-          <div style={{ width: '500px' }}>
+          <div style={{ fontSize: '36px', marginTop: '80px' }}>Fun with AI</div>
+          <div style={{ fontSize: '18px', marginTop: '10px' }}>
+            Using GPT-3 by OpenAI
+          </div>
+          <div
+            style={{ color: '#777777', textAlign: 'right', marginTop: '20px' }}
+          >
+            @satrajit/@wisebit
+          </div>
+          <div style={{ marginTop: '20px', width: '500px' }}>
             <TextField
               id='outlined-multiline-static'
               label='Input prompt for AI'
@@ -22,7 +38,20 @@ export default function Prompt() {
               rows={5}
               fullWidth
               placeholder='Start typing...'
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
             />
+          </div>
+          <div style={{ textAlign: 'right', marginTop: '20px' }}>
+            <Button
+              variant='outlined'
+              onClick={() => {
+                search();
+              }}
+            >
+              Search
+            </Button>
           </div>
         </Box>
       </div>
