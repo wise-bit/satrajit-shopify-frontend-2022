ff174b95f2122def7aa395a3e17f45a2bc3c5940
wise-bit <satrajit2016@gmail.com>
2022-05-12 23:52:05 -0400

    completed all planned features of the app

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,13 +1,75 @@
 import Box from '@mui/material/Box';
-import { TextField } from '@mui/material';
+import { Link, TextField } from '@mui/material';
 import { Button } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Response from './Response';
 
 export default function Prompt() {
-  const [input, setInput] = useState('');
+  const [showEmptyError, setShowEmptyError] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [searchHistory, setSearchHistory] = useState(() => {
+    const savedHistory = localStorage.getItem('gpt3searchhistory');
+    const initialHistory = JSON.parse(savedHistory);
+    return initialHistory || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('gpt3searchhistory', JSON.stringify(searchHistory));
+  }, [searchHistory, searchText]);
+
+  const clearResults = () => {
+    setSearchHistory([]);
+  };
+
+  const sampleInputTexts = [
+    'Write me a haiku about oranges',
+    'Write a poem about a dog wearing skis',
+    'Write me a python program to print hello world',
+    'Write me a go concurrency program',
+    'Can AI take over the world',
+    'How does GPT-3 work',
+  ];
+
+  const fillSuggestedText = () => {
+    setSearchText(
+      sampleInputTexts[Math.floor(Math.random() * sampleInputTexts.length)]
+    );
+  };
 
   const search = () => {
-    console.log(input);
+    if (searchText.length === 0) {
+      setShowEmptyError(true);
+      return;
+    }
+
+    const data = {
+      prompt: searchText,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    };
+
+    let resp;
+    fetch('https://api.openai.com/v1/engines/text-curie-001/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.REACT_APP_OPENAPI_SECRET}`,
+      },
+      body: JSON.stringify(data),
+    }).then((response) =>
+      response.json().then((obj) => {
+        if (obj && obj.choices && obj.choices.length && obj.choices[0].text) {
+          resp = obj.choices[0].text.trim();
+          searchHistory.unshift({ prompt: searchText, response: resp });
+          setSearchText('');
+        } else {
+          setShowEmptyError(true);
+        }
+      })
+    );
   };
 
   return (
@@ -21,37 +83,101 @@ export default function Prompt() {
         }}
       >
         <Box>
-          <div style={{ fontSize: '36px', marginTop: '80px' }}>Fun with AI</div>
-          <div style={{ fontSize: '18px', marginTop: '10px' }}>
-            Using GPT-3 by OpenAI
+          <div style={{ fontSize: '36px', marginTop: '80px' }}>
+            Lullaby with AI (and more)
           </div>
-          <div
-            style={{ color: '#777777', textAlign: 'right', marginTop: '20px' }}
-          >
-            @satrajit/@wisebit
+          <div style={{ fontSize: '18px', marginTop: '5px' }}>
+            Text generation using OpenAI's GPT-3
           </div>
-          <div style={{ marginTop: '20px', width: '500px' }}>
+          <div style={{ textAlign: 'right', marginTop: '20px' }}>
+            Created by{' '}
+            <Link
+              href='https://www.satrajit.ca'
+              target='_blank'
+              underline='none'
+            >
+              <span
+                style={{
+                  backgroundColor: 'black',
+                  padding: '6px',
+                  borderRadius: '6px',
+                  color: 'white',
+                }}
+              >
+                @satrajit
+              </span>
+            </Link>
+          </div>
+          <div style={{ fontStyle: 'italic', marginTop: '25px' }}>
+            Please wait 2 seconds for GPT-3 to process after pressing search
+          </div>
+          <div style={{ marginTop: '15px', width: '500px' }}>
             <TextField
               id='outlined-multiline-static'
               label='Input prompt for AI'
               multiline
               rows={5}
               fullWidth
-              placeholder='Start typing...'
+              placeholder='Write me a lullaby with AI'
               onChange={(e) => {
-                setInput(e.target.value);
+                setSearchText(e.target.value);
+                setShowEmptyError(false);
               }}
+              value={searchText}
             />
-          </div>
-          <div style={{ textAlign: 'right', marginTop: '20px' }}>
-            <Button
-              variant='outlined'
-              onClick={() => {
-                search();
+            {showEmptyError && (
+              <div style={{ color: 'red', marginTop: '8px' }}>
+                Invalid input, please try again!
+              </div>
+            )}
+
+            <div style={{ textAlign: 'right', marginTop: '20px' }}>
+              <Button
+                disableElevation
+                variant='outlined'
+                onClick={() => {
+                  fillSuggestedText();
+                }}
+                style={{ marginRight: '10px' }}
+              >
+                Suggest Text
+              </Button>
+
+              <Button
+                disableElevation
+                variant='contained'
+                onClick={() => {
+                  search();
+                }}
+              >
+                Search
+              </Button>
+            </div>
+            <div
+              style={{
+                marginTop: '50px',
+                marginBottom: '20px',
+                display: 'flex',
+                alignItems: 'end',
               }}
             >
-              Search
-            </Button>
+              <div style={{ fontSize: '28px' }}>Responses</div>
+              <div style={{ marginLeft: 'auto' }}>
+                <Button
+                  onClick={() => {
+                    clearResults();
+                  }}
+                >
+                  Clear results
+                </Button>
+              </div>
+            </div>
+            <Response items={searchHistory} key={searchHistory} />
+            {searchHistory.length === 0 && (
+              <div style={{ color: '#4c7a5d' }}>
+                No search history, try searching something!
+              </div>
+            )}
           </div>
         </Box>
       </div>
--- src/components/Response.js
+++ src/components/Response.js
@@ -1,9 +1,39 @@
 import Box from '@mui/material/Box';
 
-export default function Response() {
+export default function Response({ items }) {
   return (
     <>
-      <Box>Test</Box>
+      {items.map((item, index) => {
+        return (
+          <div key={index}>
+            <Box
+              style={{
+                backgroundColor: '#d5e8dc',
+                paddingTop: '15px',
+                paddingBottom: '15px',
+                paddingLeft: '10px',
+                paddingRight: '10px',
+                border: 'solid 2px #4c7a5d',
+                borderRadius: '4px',
+                marginBottom: '10px',
+              }}
+            >
+              <div>
+                <div style={{ fontWeight: 'bold', marginBottom: '5px' }}>
+                  Prompt:
+                </div>
+                <div>{item.prompt}</div>
+              </div>
+              <div style={{ marginTop: '10px' }}>
+                <div style={{ fontWeight: 'bold', marginBottom: '5px' }}>
+                  Response:
+                </div>
+                <div style={{ whiteSpace: 'pre-wrap' }}>{item.response}</div>
+              </div>
+            </Box>
+          </div>
+        );
+      })}
     </>
   );
 }
